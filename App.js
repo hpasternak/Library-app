64eb9bbf5f5229d47942df79aa006751ed77accf
hpasternak <71357522+hpasternak@users.noreply.github.com>
2021-08-09 15:20:52 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import TransactionScreen from "./screens/BookTransactionScreen";
+import SearchScreen from "./screens/SearchScreen";
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: { screen: TransactionScreen },
+  Search: { screen: SearchScreen }
+},
+{
+defaultNavigationOptions:({navigation})=> ({
+  tabBarIcon:()=>{
+    const routename = navigation.state.routeName;
+    if(routename === "Transaction") {
+      return(
+        <Image source = {require ("./assets/book.png")} style = {{width:30, height:30}}/>
+      )
+    }
+    else if(routename === "Search") {
+      return(
+        <Image source = {require ("./assets/searchingbook.png")} style = {{width:30, height:30}}/>
+      )
+    }
+  }
+})
+}
+);
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
